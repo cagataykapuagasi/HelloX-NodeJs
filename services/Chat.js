cfea68a9607e8fd2fd3e1689b0efb4087d153d97
Cagatay <cagatay.kapuagasi@gmail.com>
2020-03-29 17:23:08 +0300

    err

--- services/Chat.js
+++ services/Chat.js
@@ -140,7 +140,7 @@ async function newMessage({ recipientId, ...other }) {
 
     fcm.send(message, function(err, response) {
       if (err) {
-        console.log("Something has gone wrong!");
+        console.log("Something has gone wrong!", err);
       } else {
         console.log("Successfully sent with response: ", response);
       }
